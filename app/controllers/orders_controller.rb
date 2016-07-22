48f0696df7b3715186509fbb77aae908cdbb6718
Antonin Archer <antonin.archer@hec.edu>
2016-07-22 23:42:55 +0200

    Add Order methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,30 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    ?  # We'll see that in a moment.
+  end
+
+  def create
+    @order = ?  # We'll see that in a moment.
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.? # We'll see that in a moment.
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'users' => 'users#index'
-  get "orders", to: "orders#index"
+  resources :orders
 end
