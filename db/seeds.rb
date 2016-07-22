2045a7b12588454501f3165579d4e50086be5979
Antonin Archer <antonin.archer@hec.edu>
2016-07-23 00:06:22 +0200

    Create DB Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Burger.destroy_all
+burgers_attributes = [
+  {
+    name:         "Le Big Fernand",
+    price:        12,
+  },
+  {
+    name:         "Le Bartholomé",
+    price:        16,
+  },
+  {
+    name:         "Le Victor",
+    price:        10,
+  }
+]
+burgers_attributes.each { |params| Burger.create!(params) }
