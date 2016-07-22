9d50629649138a86a66438286fd81ee89e359705
Antonin Archer <antonin.archer@hec.edu>
2016-07-22 23:58:11 +0200

    create burger methods

--- app/controllers/burgers_controller.rb
+++ app/controllers/burgers_controller.rb
@@ -1,16 +1,49 @@
 class BurgersController < ApplicationController
   def index
+    @burgers = Burger.all
   end
 
   def show
+    set_burger
   end
 
   def new
   end
 
   def create
+    @burger = Burger.new(params[:burger])
+    @burger.save
+
+    redirect_to burger_path(@burger)
+  end
+
+  def edit
+    set_burger
+  end
+
+  def update
+    set_burger
+    @burger.update(params[:burger])
+
+    redirect_to burger_path(@burger)
   end
 
   def destroy
+    set_burger
+    @burger.destroy
+
+    redirect_to burgers_path
+  end
+
+  private
+
+  def set_burger
+    @burger = Burger.find(params[:id])
+  end
+
+  def burger_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:burger).permit(:name, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  get 'burgers/index'
-
-  get 'burgers/show'
-
-  get 'burgers/new'
-
-  get 'burgers/create'
-
-  get 'burgers/destroy'
-
   devise_for :users
   root to: 'pages#home'
   get 'users' => 'users#index'
   resources :orders
+  resources :burgers
 end
